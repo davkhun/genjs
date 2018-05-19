5cb622104a11f11d0bc974412d4d301298a71e23
Alex <davkhun@yandex.ru>
2018-05-19 12:48:22 +0300

    science WIP

--- js/logic/science.js
+++ js/logic/science.js
@@ -14,13 +14,107 @@ class Science {
         this.ArmyPowerScientists = 0;
         this.ScienceLevelScientists = 0;
         // прогресс по каждому направлению
-        this.PopulationDensityProgress = 0;
-        this.PopulationGrowthProgress = 0;
-        this.AgricultureProgress = 0;
-        this.MoneySystemProgress = 0;
-        this.ArmyPowerProgress = 0;
-        this.ScienceLevelProgress = 0;
+        this.PopulationDensityOverallProgress = 0;
+        this.PopulationGrowthOverallProgress = 0;
+        this.AgricultureOverallProgress = 0;
+        this.MoneySystemOverallProgress = 0;
+        this.ArmyPowerOverallProgress = 0;
+        this.ScienceLevelOverallProgress = 0;
     }
 
-    
+    // рассчет текущего прогресса в зависимости от количества ученых
+    getScienceProgress(science) {
+        switch (science) {
+            case 'populationDensity':
+                return this.PopulationDensityScientists * this.ScienceLevel;
+            case 'populationGrowth':
+                return this.PopulationGrowthScientists * this.ScienceLevel;
+            case 'agriculture':
+                return this.AgricultureScientists * this.ScienceLevel;
+            case 'moneySystem':
+                return this.MoneySystemScientists * this.ScienceLevel;
+            case 'armyPower':
+                return this.ArmyPowerScientists * this.ScienceLevel;
+            case 'scienceLevel':
+                return this.ScienceLevelScientists * this.ScienceLevel;
+        }
+    }
+
+    // вычисление оставшегося прогресса
+    getRemainingProgress(science) {
+        switch (science) {
+            case 'populationDensity':
+                return 100 - this.PopulationDensityOverallProgress;
+            case 'populationGrowth':
+                return 100 - this.PopulationDensityOverallProgress;
+            case 'agriculture':
+                return 100 - this.PopulationDensityOverallProgress;
+            case 'moneySystem':
+                return 100 - this.PopulationDensityOverallProgress;
+            case 'armyPower':
+                return 100 - this.PopulationDensityOverallProgress;
+            case 'scienceLevel':
+                return 100 - this.PopulationDensityOverallProgress;
+        }
+    }
+
+    // рассчет оставшегося количества лет для исследования (текущий прогресс + имеющийся)
+    getEstimatedResearchTime(science) {
+        let overallProgress = 0;
+        switch (science) {
+            case 'populationDensity':
+                overallProgress = this.PopulationDensityOverallProgress;
+                break;
+            case 'populationGrowth':
+                overallProgress = this.PopulationGrowthOverallProgress;
+                break;
+            case 'agriculture':
+                overallProgress = this.AgricultureOverallProgress;
+                break;
+            case 'moneySystem':
+                overallProgress = this.MoneySystemOverallProgress;
+                break;
+            case 'armyPower':
+                overallProgress = this.ArmyPowerOverallProgress;
+                break;
+            case 'scienceLevel':
+                overallProgress = this.ScienceLevelOverallProgress;
+                break;
+        }
+        return Math.round(100 / (this.getScienceProgress(science)) + overallProgress);
+    }
+
+    // установка прогресса по науке
+    setProgress(science) {
+        switch (science) {
+            case 'populationDensity':
+                this.PopulationDensityOverallProgress += getScienceProgress(science);
+                break;
+            case 'populationGrowth':
+                this.PopulationGrowthOverallProgress += getScienceProgress(science);
+                break;
+            case 'agriculture':
+                this.AgricultureOverallProgress += getScienceProgress(science);
+                break;
+            case 'moneySystem':
+                this.MoneySystemOverallProgress += getScienceProgress(science);
+                break;
+            case 'armyPower':
+                this.ArmyPowerOverallProgress += getScienceProgress(science);
+                break;
+            case 'scienceLevel':
+                this.ScienceLevelOverallProgress += getScienceProgress(science);
+                break;
+        }
+    }
+
+    // массовое обновления прогресса по науке (для ежегодного пересчета)
+    massSetProgress() {
+        this.setProgress('populationDensity');
+        this.setProgress('populationGrowth');
+        this.setProgress('agriculture');
+        this.setProgress('moneySystem');
+        this.setProgress('armyPower');
+        this.setProgress('scienceLevel');
+    }
 }
