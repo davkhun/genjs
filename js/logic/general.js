c10ad87e829eaedccc6634d37ca2ee291dca61bd
Alex <davkhun@yandex.ru>
2018-06-03 17:32:00 +0300

    add army and general view interface

--- js/logic/general.js
+++ js/logic/general.js
@@ -3,5 +3,7 @@ class General {
         this.Name = name;
         this.Age = age;
         this.Skill = skill;
+        // мораль даёт небольшой буст общей морали армии если она выше чем мораль армии. Нулевая мораль - всегда дезертирство
+        this.Morale = 100;
     }
 }
